0b4e38d00402c7a06e89b1eabef99dc04ac1ff26
fhluky <fhluky@gmail.com>
2019-08-06 16:09:55 +0700

    xxx

--- src/main/java/com/maoz/utils/ObjectConverter.java
+++ src/main/java/com/maoz/utils/ObjectConverter.java
@@ -3,10 +3,24 @@ package com.maoz.utils;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.maoz.utils.annotations.DateValueFormat;
+import com.maoz.utils.annotations.StringDelimeterToList;
 
 public class ObjectConverter {
 
 	private final static String THREAD = Thread.currentThread().getId() + " " + Thread.currentThread().getName();
+	private final static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.ENGLISH);
+	private final static Date currentDate = new Date();
+	private final static String TIME = sdf.format(currentDate);
 
 	public ObjectConverter() {
 
@@ -21,15 +35,64 @@ public class ObjectConverter {
 
 			for (Field f : fields) {
 
-				System.out.println(THREAD + " Field " + f.getName());
+				System.out.println("["+TIME+"] ["+THREAD+"] tartget Field name" + f.getName());
+				System.out.println("["+TIME+"] ["+THREAD+"] tartget Field type" + f.getType());
 
 				for (Method m : source.getClass().getMethods()) {
 					if (m.getName().startsWith("get") && m.getName().length() == f.getName().length() + 3) {
 						if (m.getName().toLowerCase().endsWith(f.getName().toLowerCase())) {
+							//setValue
 							f.setAccessible(true);
 							try {
-								f.set(newObj, m.invoke(source));
-								System.out.println(THREAD + " Value " + m.invoke(source));
+								Object value = m.invoke(source);
+								//valueFormat anno
+								DateValueFormat valueFormat = m.getAnnotation(DateValueFormat.class);
+								if(null != valueFormat) {
+									
+									String format = valueFormat.format();
+									
+									System.out.println("["+TIME+"] ["+THREAD+"] DateValueFormat " + format);
+									
+									SimpleDateFormat sdf = new SimpleDateFormat(format);
+									String valueString = String.valueOf(value);
+									try {
+										Date dateObject = sdf.parse(valueString);
+										value = dateObject;
+									} catch (ParseException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									
+									
+									
+								} 
+								
+								StringDelimeterToList transFormTo = m.getAnnotation(StringDelimeterToList.class);
+								if(null != transFormTo) {
+									String sourceType = transFormTo.sourceType();
+									String targetType = transFormTo.targetType();
+									String delimeter = transFormTo.delimeter();
+									
+									System.out.println("["+TIME+"] ["+THREAD+"] TransFormTo source " + sourceType);
+									System.out.println("["+TIME+"] ["+THREAD+"] TransFormTo target " + targetType);
+									System.out.println("["+TIME+"] ["+THREAD+"] TransFormTo delimeter " + delimeter);
+									
+									if(StringUtils.equals("java.lang.String", sourceType) && StringUtils.equals("java.util.List", targetType)) {
+										List<String> valueList = new ArrayList<String>();
+										String strValue = String.valueOf(value);
+										String[] splitedString = StringUtils.split(strValue, delimeter);
+										for (int i = 0; i < splitedString.length; i++) {
+											String s = splitedString[i];
+											valueList.add(s);
+										}
+										value = valueList;
+									}
+									
+								}
+								
+								
+								f.set(newObj, value);
+								System.out.println("["+TIME+"] ["+THREAD+"] Value " + value);
 							} catch (InvocationTargetException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
